00b1bd3337c779be5cc4e250a89b084a9e96894a
Christian Aranda <kristian.aranda@gmail.com>
2022-09-24 15:32:00 +0200

    small refactor

--- PrisionBreak/Operators.cs
+++ PrisionBreak/Operators.cs
@@ -8,7 +8,8 @@ public static class Operators
     public static IScrumbled<Box> ToScrumbledWithLoopStrategy(this IEnumerable<int> sequence) =>
         new BoxContainer(sequence.Select(x => new Box(x, x)), new LoopBoxStrategy());
 
-    public static bool IsScenarioSuccess(this IScrumbled<Box> boxes, int limit) => boxes.CheckPaths(limit.NonNegativeOrZero()).All(x => x);
+    public static bool IsScenarioSuccess(this IScrumbled<Box> boxes, int limit) => 
+        boxes.CheckPaths(limit.NonNegativeOrZero()).All(x => x);
 
     private static IEnumerable<bool> CheckPaths(this IScrumbled<Box> scrumbled, int limit)
     {
